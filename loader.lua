c08f33241d9aaee4e8a478923f85392908083f62
RealAquaVClip <osamaobada22@gmail.com>
2026-04-25 10:33:39 +0200

    Add support for skywars game mode

--- loader.lua
+++ loader.lua
@@ -3,6 +3,8 @@ if game.PlaceId == 11630038968 or game.PlaceId == 10810646982 or game.PlaceId ==
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/RealAquaVClip/LemonForRoblox/refs/heads/main/bridge_duel.lua"))()
 elseif game.PlaceId == 123941384302059 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/RealAquaVClip/LemonForRoblox/refs/heads/main/bridge_sky.lua"))()
+elseif game.PlaceId == 8768229691 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RealAquaVClip/LemonForRoblox/refs/heads/main/skywars.lua"))()
 else
 	game:GetService("Chat"):Chat(game.Players.LocalPlayer.Character:WaitForChild("Head"), "I love eating AquaVClip feces")
 end
